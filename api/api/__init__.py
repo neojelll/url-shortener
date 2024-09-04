24d7e172ac0ca4cd45bedd4ee22bf7f8909f93b7
deadonted <timofejmitusin60892@gmail.com>
2024-09-04 23:08:57 +0300

    feat: add unprepared api

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,22 +1,43 @@
-from typing import Dict
+from typing import Dict, List
 from fastapi import FastAPI
-from pydantic import BaseModel
-from enum import Enum
+from urllib.parse import urlparse
 import uvicorn
 import asyncio
+import uuid
 
+#создание приложения
+app = FastAPI(
+	title="URL Shortener API"
+)
 
-app = FastAPI()
+'''url_dict format
+url_dict = {
+    url: ...
+    prefix: ...  | default = ""
+    expiration: ... | default = 24 hours
+    Task: Task-num
+}'''
+url_dict = {}
 
+#проверка ссылки на коректность
+def is_valid_url(url: str) -> bool:
+    parsed_url = urlparse(url)
+    return all([parsed_url.scheme, parsed_url.netloc])
 
-@app.get('/')
-async def main() -> Dict[str, str]:
-	return {"name": "surname"}
+#запрос из cURL or TelegramBot
+@app.post("/v1/url/shorten")
+async def post_url(url_dict: Dict[str, str]):
+    url: str = url_dict.setdefault("url", "")
+    if is_valid_url(url):
+        task_num = uuid.uuid5(uuid.NAMESPACE_DNS, urlparse(url).netloc)
+        url_dict.update({"Task": str(task_num)})
+        return {"Task": str(task_num)}
+    return {"ERROR": "not valid url"}
 
-
-@app.post('/')
-async def sum(num1: int = 10, num2: int = 5) -> Dict[str, int]:
-	return {"sum": num1 + num2}
+#запрос из кафки
+app.get("v1/url/shorten")
+async def get_request():
+    pass
 
 
 uvicorn.run(app, host="127.0.0.1", port=8000)
