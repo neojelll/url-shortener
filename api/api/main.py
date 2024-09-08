44af0da095825fb1424141b3e3562c5d7958e39b
deadonted <timofejmitusin60892@gmail.com>
2024-09-09 02:02:19 +0300

    feat: add integration-pipeline

--- api/api/main.py
+++ api/api/main.py
@@ -33,7 +33,7 @@ async def post_url(request: ShortURLRequest):
         await asyncio.sleep(1)
         return {"task": str(task_num)}
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                        detail="don`t correct url")
+                        detail="Invalid URL")
 
 
 @app.get("/v1/url/shorten")
