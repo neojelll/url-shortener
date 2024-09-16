4d62b6185c9cbda025b106bfc0feb6d7e9a670f8
deadonted <timofejmitusin60892@gmail.com>
2024-09-16 17:54:57 +0300

    refactor: simplified interaction with servises

--- api/api/api.py
+++ api/api/api.py
@@ -4,16 +4,13 @@ import sys
 from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import RedirectResponse
 
-from kafka import KafkaProducer
-import json
+from api.message_broker import MessageBroker
+from api.db import DataBase
+from api.cache import Cache
 
 from urllib.parse import urlparse
 from pydantic import BaseModel
 import uuid
-
-from sqlalchemy import create_engine, Column, Integer, String, Text, TIMESTAMP, ForeignKey
-from sqlalchemy.orm import relationship, sessionmaker, declarative_base
-import redis
 
 
 logger.remove()
@@ -23,45 +20,6 @@ logger.add(sys.stderr, format="{time:YYYY-MM-DD at HH:mm:ss} <level>{level}</lev
 
 
 logger.add("api.log", format="{time:YYYY-MM-DD at HH:mm:ss} {level} {name}: {function}({line}) - {message}", level="DEBUG")
-
-
-USERNAME = 'your_username'
-PASSWORD = 'your_password'
-HOST = 'remote_host_address'
-PORT = '5432'
-DATABASE = 'your_database_name'
-
-
-DATABASE_URL = f'postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
-
-
-Base = declarative_base()
-
-# Определяем модель LongUrl
-class LongUrl(Base):
-    __tablename__ = 'long_url'
-
-    long_id = Column(Integer, primary_key=True)
-    long_value = Column(Text, unique=True, nullable=False)
-
-# Определяем модель ShortUrl
-class ShortUrl(Base):
-    __tablename__ = 'short_url'
-
-    short_id = Column(String(20), primary_key=True)
-    short_value = Column(Text, nullable=False)
-    created_at = Column(TIMESTAMP)
-
-# Определяем модель UrlMapping
-class UrlMapping(Base):
-    __tablename__ = 'url_mapping'
-
-    short_id = Column(String(20), ForeignKey('short_url.short_id'), primary_key=True)
-    long_id = Column(Integer, ForeignKey('long_url.long_id'), nullable=False)
-    expiration_date = Column(TIMESTAMP)
-
-    short_url = relationship('ShortUrl', backref='url_mappings')
-    long_url = relationship('LongUrl', backref='url_mappings')
 
 
 def is_valid_url(url: str) -> bool:
@@ -82,9 +40,9 @@ app = FastAPI(title="URL Shortener API")
 
 
 @app.post("/v1/url/shorten")
-async def post_url(request: ShortURLRequest):
-    logger.debug(f"Start post request... params: {repr(request)}")
-    url = request.url
+async def post_url(data: ShortURLRequest):
+    logger.debug(f"Start post request... params: {repr(data)}")
+    url = data.url
     if not is_valid_url(url):
         logger.error("Post request error. ERROR INFO: Invalid URL")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
@@ -92,14 +50,8 @@ async def post_url(request: ShortURLRequest):
     task_num = uuid.uuid5(uuid.NAMESPACE_DNS, urlparse(url).netloc)
     returned_value = {"task": str(task_num)}
 
-    producer = KafkaProducer(
-        bootstrap_servers="localhost:9092",
-        value_serializer=lambda x: json.dumps(x).encode("utf-8")
-        )
-    logger.debug(f"Send data to message-broker... params: {repr(request)}")
-    await producer.send("topic_name", request)
-    producer.flush()
-    producer.close()
+    with MessageBroker() as broker:
+        broker.send_data("my_topic", data)
 
     logger.debug(f"Post request completed. returned: {repr(returned_value)}")
     return returned_value
@@ -113,35 +65,20 @@ async def get_request():
 @app.get("/{short_url}")
 async def transport_to_long_url(short_url: str):
     logger.debug(f"Start redirect response... params: {repr(short_url)}")
-
-    cache = redis.Redis(
-    host='localhost',
-    port=6379,
-    db=0,# Номер базы данных (по умолчанию 0)
-    decode_responses=True# Автоматически декодировать байтовые строки в строки
-    )
-
-    if cache.exists(short_url):
-        long_url = cache.get(short_url)
-    else:
-        engine = create_engine(DATABASE_URL)
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        def get_long_value(short_value):
-            result = (
-        		session.query(LongUrl)
-        		.join(UrlMapping)
-        		.join(ShortUrl)
-        		.filter(ShortUrl.short_value == short_value)
-        		.first()
-    		)
-            return result.long_value if result else None
-
-        long_url = get_long_value(short_value=short_url)
-        session.close()
-
-    cache.set(short_url, long_url) #type: ignore
+	
+    with Cache() as cache:
+        check = cache.check(short_url)
+        if isinstance(check, str):
+            long_url = check
+        else:
+            with DataBase() as database:
+                long_url = database.get_long_url(short_url)
+        
+    	cache.set(short_url, long_url) #type: ignore
+    
+    if long_url is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="URL is not valid")
 
     logger.debug(f"Redirect response completed. returned: Redirect to {repr("long_url")}")
     return RedirectResponse(url="http://github.com", status_code=status.HTTP_302_FOUND) #type: ignore
