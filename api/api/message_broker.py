9263eb65ec91274f6bc6e3e234280d681de8ec40
deadonted <timofejmitusin60892@gmail.com>
2024-09-16 17:49:17 +0300

    feat: add adapter to message broker

---
+++ api/api/message_broker.py
@@ -0,0 +1,23 @@
+from kafka import KafkaProducer
+from api.api import ShortURLRequest 
+from loguru import logger
+import json
+
+
+class MessageBroker(object):
+	def __init__(self):
+		self.producer = KafkaProducer(
+			bootstrap_servers="localhost:9092",
+			value_serializer=lambda x: json.dumps(x).encode("utf-8")
+		)
+		
+	def __enter__(self):
+		return self
+
+	def send_data(self, topic: str, data: ShortURLRequest):
+		logger.debug(f"Send data to message-broker... params: {repr(data)}")
+		self.producer.send(topic, data)
+
+	def __exit__(self, exc_type, exc_value, traceback):
+		self.producer.flush()
+		self.producer.close()
