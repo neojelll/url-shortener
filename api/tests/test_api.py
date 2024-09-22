b12f103f7d9f04aab506be9bdce401e4c193c3c0
deadonted <timofejmitusin60892@gmail.com>
2024-09-22 15:11:59 +0300

    refactor: two part tests

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -15,6 +15,20 @@ async def mock_broker(mocker):
 	mock_broker_instance.__aenter__.return_value = mock_broker_instance
 	return mock_broker_instance
 
+@pytest_asyncio.fixture
+async def mock_cache(mocker):
+	mock_cache = mocker.patch("api.api.Cache", autospec=True)
+	mock_cache_instance = mock_cache.return_value
+	mock_cache_instance.__aenter__.return_value = mock_cache_instance
+	return mock_cache_instance
+
+@pytest_asyncio.fixture
+async def mock_db(mocker):
+	mock_db = mocker.patch("api.api.DataBase", autospec=True)
+	mock_db_instance = mock_db.return_value
+	mock_db_instance.__aenter__.return_value = mock_db_instance
+	return mock_db_instance
+
 @pytest.fixture
 def client():
 	return TestClient(app)
@@ -39,110 +53,69 @@ async def test_post_correct_url(mock_broker, client, data, error, in_json):
 	mock_broker_instance.send_data.assert_awaited_with("topic_name", data)
 
 @pytest.mark.asyncio
-async def test_get_request_cache_hit(mocker, client):
-	mock_cache = mocker.patch("api.api.Cache", autospec=True)
-	mock_cache_instance = mock_cache.return_value
-	mock_cache_instance.__aenter__.return_value = mock_cache_instance
+async def test_get_request_cache_hit(mock_cache, client):
+	mock_cache_instance = mock_cache
 	mock_cache_instance.check = AsyncMock(return_value=LONG_URL)
-
 	response = client.get(f"/v1/url/shorten?short_url={SHORT_URL}")
 	assert response.status_code == status.HTTP_200_OK
 	assert "long_url" in response.json()
-
 	mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
 
-
 @pytest.mark.asyncio
-async def test_get_cache_miss(mocker, client):
-	mock_cache = mocker.patch("api.api.Cache", autospec=True)
-	mock_cache_instance = mock_cache.return_value
-	mock_cache_instance.__aenter__.return_value = mock_cache_instance
+async def test_get_cache_miss(mock_cache, mock_db, client):
+	mock_cache_instance = mock_cache
 	mock_cache_instance.check = AsyncMock(return_value=None)
 	mock_cache_instance.set = AsyncMock()
-
-	mock_db = mocker.patch("api.api.DataBase", autospec=True)
-	mock_db_instance = mock_db.return_value
-	mock_db_instance.__aenter__.return_value = mock_db_instance
+	mock_db_instance = mock_db
 	mock_db_instance.get_long_url = AsyncMock(return_value=LONG_URL)
-
 	response = client.get(f"/v1/url/shorten?short_url={SHORT_URL}")
 	assert response.status_code == status.HTTP_200_OK
 	assert "long_url" in response.json()
-
 	mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
 	mock_db_instance.get_long_url.assert_awaited_once_with(SHORT_URL)
 	mock_cache_instance.set.assert_awaited_once_with(SHORT_URL, LONG_URL)
 
-
 @pytest.mark.asyncio
-async def test_get_no_db_record(mocker, client):
-	mock_cache = mocker.patch("api.api.Cache", autospec=True)
-	mock_cache_instance = mock_cache.return_value
-	mock_cache_instance.__aenter__.return_value = mock_cache_instance
+async def test_get_no_db_record(mock_cache, mock_db, client):
+	mock_cache_instance = mock_cache
 	mock_cache_instance.check = AsyncMock(return_value=None)
-
-	mock_db = mocker.patch("api.api.DataBase", autospec=True)
-	mock_db_instance = mock_db.return_value
-	mock_db_instance.__aenter__.return_value = mock_db_instance
+	mock_db_instance = mock_db
 	mock_db_instance.get_long_url = AsyncMock(return_value=None)
-
 	response = client.get(f"/v1/url/shorten?short_url={SHORT_URL}")
 	assert response.status_code == status.HTTP_404_NOT_FOUND
 	assert "detail" in response.json()
-
 	mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
 	mock_db_instance.get_long_url.assert_awaited_once_with(SHORT_URL)
 
-
 @pytest.mark.asyncio
-async def test_redirect_cache_hit(mocker, client):
-	mock_cache = mocker.patch("api.api.Cache", autospec=True)
-	mock_cache_instance = mock_cache.return_value
-	mock_cache_instance.__aenter__.return_value = mock_cache_instance
+async def test_redirect_cache_hit(mock_cache, client):
+	mock_cache_instance = mock_cache
 	mock_cache_instance.check = AsyncMock(return_value=LONG_URL)
-
 	response = client.get(f"/{SHORT_URL}", follow_redirects=False)
 	assert response.status_code == status.HTTP_302_FOUND
-
 	mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
 
-
 @pytest.mark.asyncio
-async def test_redirect_cache_miss(mocker, client):
-	mock_cache = mocker.patch("api.api.Cache", autospec=True)
-	mock_cache_instance = mock_cache.return_value
-	mock_cache_instance.__aenter__.return_value = mock_cache_instance
+async def test_redirect_cache_miss(mock_cache, mock_db, client):
+	mock_cache_instance = mock_cache
 	mock_cache_instance.check = AsyncMock(return_value=None)
 	mock_cache_instance.set = AsyncMock()
-
-	mock_db = mocker.patch("api.api.DataBase", autospec=True)
-	mock_db_instance = mock_db.return_value
-	mock_db_instance.__aenter__.return_value = mock_db_instance
+	mock_db_instance = mock_db
 	mock_db_instance.get_long_url = AsyncMock(return_value=LONG_URL)
-
 	response = client.get(f"/{SHORT_URL}", follow_redirects=False)
 	assert response.status_code == status.HTTP_302_FOUND
-
 	mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
 	mock_db_instance.get_long_url.assert_awaited_once_with(SHORT_URL)
 	mock_cache_instance.set.assert_awaited_once_with(SHORT_URL, LONG_URL)
 
-
 @pytest.mark.asyncio
-async def test_redirect_no_db_record(mocker, client):
-	mock_cache = mocker.patch("api.api.Cache", autospec=True)
-	mock_cache_instance = mock_cache.return_value
-	mock_cache_instance.__aenter__.return_value = mock_cache_instance
+async def test_redirect_no_db_record(mock_cache, mock_db, client):
+	mock_cache_instance = mock_cache
 	mock_cache_instance.check = AsyncMock(return_value=None)
-
-	mock_db = mocker.patch("api.api.DataBase", autospec=True)
-	mock_db_instance = mock_db.return_value
-	mock_db_instance.__aenter__.return_value = mock_db_instance
+	mock_db_instance = mock_db
 	mock_db_instance.get_long_url = AsyncMock(return_value=None)
-
 	response = client.get(f"/{SHORT_URL}", follow_redirects=False)
 	assert response.status_code == status.HTTP_404_NOT_FOUND
 	assert "detail" in response.json()
-
 	mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
 	mock_db_instance.get_long_url.assert_awaited_once_with(SHORT_URL)
