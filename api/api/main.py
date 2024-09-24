03b1dcc59c189af3a18c8d1842399d5f3d8f264b
deadonted <timofejmitusin60892@gmail.com>
2024-09-24 10:18:26 +0300

    refactor: modified dist

--- api/api/main.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-from api import app #type: ignore
-import uvicorn
-
-    
-if __name__ == "__main__":
-    logger.info("Starting the server...")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
