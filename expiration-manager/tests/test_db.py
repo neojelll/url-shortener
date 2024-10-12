1e117ec6bf86a4cb4f2cba95731eb33fb17898de
deadonted <timofejmitusin60892@gmail.com>
2024-10-12 14:37:21 +0300

    feat: add manager test and finished db tests

--- expiration-manager/tests/test_db.py
+++ expiration-manager/tests/test_db.py
@@ -1,12 +1,12 @@
 import pytest
 import pytest_asyncio
 from unittest.mock import AsyncMock
-from .expiration_manager.db import DataBase
+from expiration_manager.db import DataBase
 
 @pytest_asyncio.fixture
 async def mock_db(mocker):
-    mocker.patch("api.db.create_async_engine", autospec=True)
-    mock_sessionmaker = mocker.patch("api.db.async_sessionmaker", autospec=True)
+    mocker.patch("expiration_manager.db.create_async_engine", autospec=True)
+    mock_sessionmaker = mocker.patch("expiration_manager.db.async_sessionmaker", autospec=True)
     mock_session = AsyncMock()
     mock_sessionmaker.return_value = AsyncMock(return_value=mock_session)
     db = DataBase()
@@ -26,25 +26,19 @@ async def test_aenter(mock_db):
 @pytest.mark.asyncio
 async def test_delete_after_time(mock_db):
     db, mock_session = mock_db
-
     mock_session.execute = AsyncMock()
     mock_session.commit = AsyncMock()
-
     mock_session.execute.return_value.rowcount = 5
-
     result = await db.delete_after_time()
+    mock_session.commit.assert_awaited_once()
 
     assert result == 5
-    mock_session.commit.assert_awaited_once()
 
 @pytest.mark.asyncio
 async def test_delete_after_time_error(mock_db):
     db, mock_session = mock_db
-
     mock_session.execute = AsyncMock(side_effect=Exception("DB error"))
     mock_session.rollback = AsyncMock()
-
     result = await db.delete_after_time()
-
     assert result == 0
     mock_session.rollback.assert_awaited_once()
---
+++ expiration-manager/tests/test_manager.py
@@ -0,0 +1,13 @@
+from expiration_manager.manager import expiration_manager
+from unittest.mock import AsyncMock
+import pytest
+
+@pytest.mark.asyncio
+async def test_expiration_manager(mocker):
+    mock_db_class = mocker.patch("expiration_manager.manager.DataBase", autospec=True)
+    db_instance = mock_db_class.return_value
+    db_instance.__aenter__.return_value = db_instance
+    db_instance.delete_after_time = AsyncMock(return_value=5)
+    result = await expiration_manager()
+    assert result == 5
+    db_instance.delete_after_time.assert_awaited_once()
