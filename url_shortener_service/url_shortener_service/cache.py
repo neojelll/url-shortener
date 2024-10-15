1dbb19c54963cf2a7ed4291a7b8f3aa59c03043e
deadonted <timofejmitusin60892@gmail.com>
2024-10-15 22:17:00 +0300

    refactor: ruff format

--- url_shortener_service/url_shortener_service/cache.py
+++ url_shortener_service/url_shortener_service/cache.py
@@ -8,24 +8,24 @@ configure_logger()
 
 
 def ttl():
-	result = os.environ["CACHE_TTL"]
-	return int(result)
+    result = os.environ["CACHE_TTL"]
+    return int(result)
 
 
-class Cache():
-	def __init__(self):
-		self.session = Redis(host="localhost", port=6379, db=0, decode_responses=True)
+class Cache:
+    def __init__(self):
+        self.session = Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
-	async def __aenter__(self):
-		return self
-	
-	async def create_recording(self, short_url, long_url, expiration):
-		try:
-			ttl_value = min(expiration // 3600, ttl())
-			await self.session.set(short_url, long_url, ex=ttl_value)
-			logger.info("write successfully to cache")
-		except Exception as e:
-			logger.error(f"Error when writing data to the cache: {e}")
+    async def __aenter__(self):
+        return self
 
-	async def __aexit__(self, exc_type, exc_value, traceback):
-		await self.session.aclose()
+    async def create_recording(self, short_url, long_url, expiration):
+        try:
+            ttl_value = min(expiration // 3600, ttl())
+            await self.session.set(short_url, long_url, ex=ttl_value)
+            logger.info("write successfully to cache")
+        except Exception as e:
+            logger.error(f"Error when writing data to the cache: {e}")
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.session.aclose()
