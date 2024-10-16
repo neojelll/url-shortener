9cf76424a2414871109c4245bab38f1c74bf3878
deadonted <timofejmitusin60892@gmail.com>
2024-10-16 19:44:46 +0300

    feat: add message_broker.py

---
+++ url_shortener_service/url_shortener_service/message_broker.py
@@ -0,0 +1,34 @@
+from aiokafka import AIOKafkaConsumer
+from .logger import configure_logger
+from loguru import logger
+import asyncio
+
+
+configure_logger()
+
+
+class MessageBroker:
+    def __init__(self):
+        self.consumer = AIOKafkaConsumer(
+            "my_topic",
+            bootstrap_servers="localhost:9092",
+            loop=asyncio.get_event_loop(),
+            group_id="group_1",
+        )
+
+    async def __aenter__(self):
+        await self.consumer.start()
+        return self
+
+    async def consume_data(self):
+        try:
+            while True:
+                async for msg in self.consumer:
+                    if msg.value is not None:
+                        yield msg.value.decode("utf-8")
+                    logger.warning("Received a message is None value")
+        except Exception as e:
+            logger.error(f"Error when consume data from kafka: {e}")
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.consumer.stop()
