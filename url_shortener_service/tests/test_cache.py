84a59702fcab06acf698bb60006d7dc3557fa8d7
deadonted <timofejmitusin60892@gmail.com>
2024-10-16 19:43:51 +0300

    refactor: ruff format

--- url_shortener_service/tests/test_cache.py
+++ url_shortener_service/tests/test_cache.py
@@ -11,43 +11,43 @@ EXPIRATION = 5
 
 @pytest_asyncio.fixture
 async def mock_cache(mocker):
-	mocker.patch("url_shortener_service.cache.min")
-	mocker.patch("url_shortener_service.cache.ttl", autospec=True)
-	mock_redis = mocker.patch("url_shortener_service.cache.Redis", autospec=True)
-	mock_session = AsyncMock()
-	mock_redis.return_value = mock_session
-	cache = Cache()
-	async with cache as cache_instance:
-		yield cache_instance, mock_session
+    mocker.patch("url_shortener_service.cache.min")
+    mocker.patch("url_shortener_service.cache.ttl", autospec=True)
+    mock_redis = mocker.patch("url_shortener_service.cache.Redis", autospec=True)
+    mock_session = AsyncMock()
+    mock_redis.return_value = mock_session
+    cache = Cache()
+    async with cache as cache_instance:
+        yield cache_instance, mock_session
 
 
 def test_ttl(mocker):
-	mocker.patch("url_shortener_service.cache.os", autospec=True)
-	ttl()
+    mocker.patch("url_shortener_service.cache.os", autospec=True)
+    ttl()
 
 
 @pytest.mark.asyncio
 async def test_init(mock_cache):
-	cache, _ = mock_cache
-	assert isinstance(cache, Cache)
+    cache, _ = mock_cache
+    assert isinstance(cache, Cache)
 
 
 @pytest.mark.asyncio
 async def test_aenter(mock_cache):
-	cache, _ = mock_cache
-	assert isinstance(cache, Cache)
+    cache, _ = mock_cache
+    assert isinstance(cache, Cache)
 
 
 @pytest.mark.asyncio
 async def test_create_recording(mock_cache):
-	cache, mock_session = mock_cache
-	mock_session.set = AsyncMock()
-	await cache.create_recording(SHORT_URL, LONG_URL, EXPIRATION)
-	mock_session.set.assert_awaited_once()
+    cache, mock_session = mock_cache
+    mock_session.set = AsyncMock()
+    await cache.create_recording(SHORT_URL, LONG_URL, EXPIRATION)
+    mock_session.set.assert_awaited_once()
 
 
 @pytest.mark.asyncio
 async def test_create_recording_error(mock_cache):
-	cache, mock_session = mock_cache
-	mock_session.set = AsyncMock(side_effect=Exception("Cache error"))
-	await cache.create_recording(SHORT_URL, LONG_URL, EXPIRATION)
+    cache, mock_session = mock_cache
+    mock_session.set = AsyncMock(side_effect=Exception("Cache error"))
+    await cache.create_recording(SHORT_URL, LONG_URL, EXPIRATION)
--- url_shortener_service/tests/test_db.py
+++ url_shortener_service/tests/test_db.py
@@ -1,7 +1,7 @@
 import pytest
 import pytest_asyncio
 from unittest.mock import AsyncMock, MagicMock
-from url_shortener_service.db import DataBase, LongUrl, ShortUrl, UrlMapping
+from url_shortener_service.db import DataBase
 
 
 SHORT_URL = "shortener.com"
@@ -11,38 +11,42 @@ EXPIRATION = 5
 
 @pytest_asyncio.fixture
 async def mock_db(mocker):
-	mocker.patch("url_shortener_service.db.create_async_engine", autospec=True)
-	mock_sessionmaker = mocker.patch("url_shortener_service.db.async_sessionmaker", autospec=True)
-	mock_session = AsyncMock()
-	mock_sessionmaker.return_value = MagicMock(return_value=mock_session)
-	db = DataBase()
-	async with db as db_instance:
-		yield db_instance, mock_session
+    mocker.patch("url_shortener_service.db.create_async_engine", autospec=True)
+    mock_sessionmaker = mocker.patch(
+        "url_shortener_service.db.async_sessionmaker", autospec=True
+    )
+    mock_session = AsyncMock()
+    mock_sessionmaker.return_value = MagicMock(return_value=mock_session)
+    db = DataBase()
+    async with db as db_instance:
+        yield db_instance, mock_session
 
 
 @pytest.mark.asyncio
 async def test_init(mock_db):
-	db, _ = mock_db
-	isinstance(db, DataBase)
+    db, _ = mock_db
+    isinstance(db, DataBase)
+
 
 @pytest.mark.asyncio
 async def test_aenter(mock_db):
-	db, _ = mock_db
-	isinstance(db, DataBase)
+    db, _ = mock_db
+    isinstance(db, DataBase)
+
 
 @pytest.mark.asyncio
 async def test_create_recording(mock_db):
-	db, mock_session = mock_db
-	mock_session.add = MagicMock()
-	mock_session.commit = AsyncMock()
-	await db.create_recording(LONG_URL, SHORT_URL, EXPIRATION)
-	assert mock_session.add.call_count == 3
-	mock_session.commit.assert_awaited()
+    db, mock_session = mock_db
+    mock_session.add = MagicMock()
+    mock_session.commit = AsyncMock()
+    await db.create_recording(LONG_URL, SHORT_URL, EXPIRATION)
+    assert mock_session.add.call_count == 3
+    mock_session.commit.assert_awaited()
 
 
 @pytest.mark.asyncio
 async def test_create_recording_error(mock_db):
-	db, mock_session = mock_db
-	mock_session.add = MagicMock(side_effect=Exception("DB error"))
-	mock_session.commit = AsyncMock()
-	await db.create_recording(LONG_URL, SHORT_URL, EXPIRATION)
+    db, mock_session = mock_db
+    mock_session.add = MagicMock(side_effect=Exception("DB error"))
+    mock_session.commit = AsyncMock()
+    await db.create_recording(LONG_URL, SHORT_URL, EXPIRATION)
