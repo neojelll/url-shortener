3578dd964769c395f89f5018d83adcbd8527785a
deadonted <timofejmitusin60892@gmail.com>
2024-11-06 14:37:43 +0300

    fix: api and test api

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -8,6 +8,7 @@ import pytest
 SHORT_URL = "shortener.com"
 LONG_URL = "http://shortener.com/long"
 EXPIRATION = 300
+TASK_NUM = "320i3039029039"
 
 
 @pytest_asyncio.fixture
@@ -72,43 +73,38 @@ async def test_post_correct_url(mock_broker, client, data, error, in_json):
 
 
 @pytest.mark.asyncio
-async def test_get_request_cache_hit(mock_cache, client):
-    mock_cache_instance = mock_cache
-    mock_cache_instance.check = AsyncMock(return_value=LONG_URL)
-    response = client.get(f"/v1/url/shorten?short_url={SHORT_URL}")
-    assert response.status_code == status.HTTP_200_OK
-    assert "long_url" in response.json()
-    mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
-
-
-@pytest.mark.asyncio
-async def test_get_cache_miss(mock_cache, mock_db, client):
-    mock_cache_instance = mock_cache
-    mock_cache_instance.check = AsyncMock(return_value=None)
+async def test_get_db_hit(mock_db, client):
     mock_db_instance = mock_db
-    mock_db_instance.get_long_url = AsyncMock(return_value=LONG_URL)
+    mock_db_instance.get_short_url = AsyncMock(return_value=SHORT_URL)
     mock_db_instance.get_expiration = AsyncMock(return_value=EXPIRATION)
-    response = client.get(f"/v1/url/shorten?short_url={SHORT_URL}")
+    response = client.get(f"/v1/url/shorten?task_num={TASK_NUM}")
     assert response.status_code == status.HTTP_200_OK
-    assert "long_url" in response.json()
-    mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
-    mock_db_instance.get_long_url.assert_awaited_once_with(SHORT_URL)
+    assert "short_url" in response.json()
+    mock_db_instance.get_short_url.assert_awaited_once_with(TASK_NUM)
     mock_db_instance.get_expiration.assert_awaited_once_with(SHORT_URL)
-    mock_cache_instance.set.assert_awaited_once_with(SHORT_URL, LONG_URL, EXPIRATION)
 
 
 @pytest.mark.asyncio
-async def test_get_no_db_record(mock_cache, mock_db, client):
-    mock_cache_instance = mock_cache
-    mock_cache_instance.check = AsyncMock(return_value=None)
+async def test_get_no_short_url(mock_db, client):
     mock_db_instance = mock_db
-    mock_db_instance.get_long_url = AsyncMock(return_value=None)
+    mock_db_instance.get_short_url = AsyncMock(return_value=None)
     mock_db_instance.get_expiration = AsyncMock(return_value=EXPIRATION)
-    response = client.get(f"/v1/url/shorten?short_url={SHORT_URL}")
+    response = client.get(f"/v1/url/shorten?task_num={TASK_NUM}")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert "detail" in response.json()
-    mock_cache_instance.check.assert_awaited_once_with(SHORT_URL)
-    mock_db_instance.get_long_url.assert_awaited_once_with(SHORT_URL)
+    mock_db_instance.get_short_url.assert_awaited_once_with(TASK_NUM)
+
+
+@pytest.mark.asyncio
+async def test_get_no_expiration(mock_db, client):
+    mock_db_instance = mock_db
+    mock_db_instance.get_short_url = AsyncMock(return_value=SHORT_URL)
+    mock_db_instance.get_expiration = AsyncMock(return_value=None)
+    response = client.get(f"/v1/url/shorten?task_num={TASK_NUM}")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert "detail" in response.json()
+    mock_db_instance.get_short_url.assert_awaited_once_with(TASK_NUM)
+    mock_db_instance.get_expiration.assert_awaited_once_with(SHORT_URL)
 
 
 @pytest.mark.asyncio
