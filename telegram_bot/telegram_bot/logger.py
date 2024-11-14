806b4d49fca95d88387ec6fa9293177579b3006a
deadonted <timofejmitusin60892@gmail.com>
2024-11-14 23:35:08 +0300

    fix: logger.py

--- telegram_bot/telegram_bot/logger.py
+++ telegram_bot/telegram_bot/logger.py
@@ -1,5 +1,5 @@
-from loguru import logger
 import sys
+from loguru import logger
 
 
 def configure_logger():
@@ -7,12 +7,12 @@ def configure_logger():
 
     logger.add(
         sys.stderr,
-        format="{time:YYYY-MM-DD at HH:mm:ss} <level>{level}</level> <red>{name}</red>: <red>{function}</red>({line}) - <cyan>{message}</cyan>",
-        level="DEBUG",
+        format='{time:YYYY-MM-DD at HH:mm:ss} <level>{level}</level> <red>{name}</red>: <red>{function}</red>({line}) - <cyan>{message}</cyan>',
+        level='DEBUG',
     )
 
     logger.add(
-        "bot.log",
-        format="{time:YYYY-MM-DD at HH:mm:ss} {level} {name}: {function}({line}) - {message}",
-        level="DEBUG",
+        'bot.log',
+        format='{time:YYYY-MM-DD at HH:mm:ss} {level} {name}: {function}({line}) - {message}',
+        level='DEBUG',
     )
