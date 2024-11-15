fc23399e92bb36581debbb08f0a69abebe2cc3e8
neojelll <155233969+neojelll@users.noreply.github.com>
2024-11-15 19:22:26 +0300

    fix(api): transition to .env (#43)

--- api/api/models.py
+++ api/api/models.py
@@ -8,22 +8,22 @@ class Base(AsyncAttrs, DeclarativeBase):
 
 
 class LongUrl(Base):
-    __tablename__ = "long_url"
+    __tablename__ = 'long_url'
     long_id = Column(Integer, primary_key=True)
     long_value = Column(String(250), unique=True, nullable=False)
 
 
 class ShortUrl(Base):
-    __tablename__ = "short_url"
+    __tablename__ = 'short_url'
     short_id = Column(Integer, primary_key=True)
     short_value = Column(String(250), nullable=False)
 
 
 class UrlMapping(Base):
-    __tablename__ = "url_mapping"
-    short_id = Column(Integer, ForeignKey("short_url.short_id"), primary_key=True)
-    short_url = relationship("ShortUrl", backref="url_mappings")
-    long_id = Column(Integer, ForeignKey("long_url.long_id"), nullable=False)
-    long_url = relationship("LongUrl", backref="url_mappings")
+    __tablename__ = 'url_mapping'
+    short_id = Column(Integer, ForeignKey('short_url.short_id'), primary_key=True)
+    short_url = relationship('ShortUrl', backref='url_mappings')
+    long_id = Column(Integer, ForeignKey('long_url.long_id'), nullable=False)
+    long_url = relationship('LongUrl', backref='url_mappings')
     expiration = Column(Integer, nullable=False)
     date = Column(TIMESTAMP, nullable=False)
