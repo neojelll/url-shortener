d51b1074a2faa0858f123e2ffa674890d7ef50e2
deadonted <timofejmitusin60892@gmail.com>
2024-11-23 13:53:23 +0300

    test

--- architecture/architecture/c4-model.py
+++ architecture/architecture/c4-model.py
@@ -61,12 +61,6 @@ with Diagram(filename=file_path, show=False, direction='TB', graph_attr=graph_at
                 description='Stores original and shortened URLs\nStores expiration',
             )
 
-            cache = Container(
-                name='Cache',
-                technology='Redis',
-                description='Stored frequently requested URLs',
-            )
-
             expiration_manager = Container(
                 name='Expiration Manager',
                 technology='Python',
@@ -93,8 +87,6 @@ with Diagram(filename=file_path, show=False, direction='TB', graph_attr=graph_at
 
         api >> Relationship('try to request long url from db cache') << database
 
-        api >> Relationship('try to request long url from db cache') << cache
-
         (
             broker
             >> Relationship('consume long URL for topic and send short URL in topic')
@@ -107,10 +99,4 @@ with Diagram(filename=file_path, show=False, direction='TB', graph_attr=graph_at
             >> database
             << Relationship('deleted continue expirated URLs')
             << expiration_manager
-        )
-
-        (
-            shortener_service
-            >> Relationship('create records and checking the short URL for existence')
-            << cache
         )
