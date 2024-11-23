32eadd740307d076d13adb12d8b9ef82e0c927dc
deadonted <timofejmitusin60892@gmail.com>
2024-11-23 11:59:40 +0300

    feat: update diagram

--- architecture/architecture/c4-model.py
+++ architecture/architecture/c4-model.py
@@ -1,99 +1,116 @@
 from diagrams import Diagram
-from diagrams.c4 import Person, Container, Relationship, SystemBoundary, System, Database
+from diagrams.c4 import (
+    Person,
+    Container,
+    Relationship,
+    SystemBoundary,
+    System,
+    Database,
+)
 
 
 graph_attr = {
-	"splines": "spline",
+    'splines': 'spline',
 }
 
 
-with Diagram(show=False, direction='TB', graph_attr=graph_attr):
-	user = Person(
-		name='User for URL Shortener',
-	)
-
-	curl = System(
-			name='curl',
-			description='',
-			external=True
-		)
+file_path = '/Users/timofei/My_Project/architecture/diagrams/container-diagram'
 
 
-	with SystemBoundary('URL Shortener'):
-		telegram_bot = System(
-			name='Telegram Bot',
-			description='',
-			external=True,
-		)
+with Diagram(filename=file_path, show=False, direction='TB', graph_attr=graph_attr):
+    user = Person(
+        name='URL Shortener user',
+        description='user who wants to shorten the URL',
+    )
 
+    curl = System(
+        name='curl',
+        description='utility for sending [HTTP] requests, the user will need to directly interact with the URL Shortener API',
+        external=True,
+    )
 
-		with SystemBoundary('Containers'):
-			api = Container(
-				name='APIGateway',
-				technology='FastAPI',
-				description='Handles and routes [HTTP] requests',
-			)
+    with SystemBoundary('URL Shortener'):
+        telegram_bot = System(
+            name='Telegram Bot',
+            description='Telegram bot for a user-friendly interface for using URL Shortener',
+            external=True,
+        )
 
-			broker = Container(
-				name='Message Queue',
-				technology='Kafka',
-				description='Handles event routing and delivery\nProcesses URL generation requests',
-			)
+        with SystemBoundary('Containers'):
+            api = Container(
+                name='APIGateway',
+                technology='FastAPI',
+                description='Handles and routes [HTTP] requests\nRedirect URLs',
+            )
 
-			shortener_service = Container(
-				name='Shortener Service',
-				technology='Python',
-				description='Shorten URLs\nRedirect URLs',
-			)
+            broker = Container(
+                name='Message Queue',
+                technology='Kafka',
+                description='Handles event routing and delivery\nProcesses URL generation requests',
+            )
 
-			database = Database(
-				name='Databse',
-				technology='PostgreSQL',
-				description='Stores original and shortened URLs\nStores expiration',
-			)
+            shortener_service = Container(
+                name='Shortener Service',
+                technology='Python',
+                description='Shorten URLs',
+            )
 
-			cache = Container(
-				name='Cache',
-				technology='Redis',
-				description='Stored frequently requested URLs',
-			)
+            database = Database(
+                name='Databse',
+                technology='PostgreSQL',
+                description='Stores original and shortened URLs\nStores expiration',
+            )
 
-			expiration_manager = Container(
-				name='Expiration Manager',
-				technology='Python',
-				description='Checks for expired URLs in URL Database\nRemoves them',
-			)
+            cache = Container(
+                name='Cache',
+                technology='Redis',
+                description='Stored frequently requested URLs',
+            )
 
-			analytics_service = Container(
-				name='Analytics Service',
-				technology='Python',
-				description='Track usage\nGenerate reports',
-			)
+            expiration_manager = Container(
+                name='Expiration Manager',
+                technology='Python',
+                description='Checks for expired URLs in URL Database\nRemoves them',
+            )
 
-			analytics_database = Container(
-				name='Analytics Database',
-				technology='Prometheus',
-				description='Stores usage data'
-			)
+        (
+            user
+            >> Relationship('Uses URL Shortener', style='bold')
+            << telegram_bot
+            >> Relationship('sending long URL')
+            << api
+        )
 
+        (
+            user
+            >> Relationship('Uses URL Shortener', style='bold')
+            << curl
+            >> Relationship('sending long URL')
+            << api
+            >> Relationship('sending long URL in topic and consume long URL for topic')
+            << broker
+        )
 
-		user >> Relationship('Uses URL Shortener') << telegram_bot \
-		>> Relationship('sending data') << api
+        api >> Relationship('try to request long url from db cache') << database
 
-		user >> Relationship('Uses URL Shortener') << curl \
-		>> Relationship('sending data') << api \
-		>> Relationship('sending data') << broker
+        api >> Relationship('try to request long url from db cache') << cache
 
-		api >> Relationship('try to request long url from db cache') << database
+        (
+            broker
+            >> Relationship('consume long URL for topic and send short URL in topic')
+            << shortener_service
+        )
 
-		api >> Relationship('try to request long url from db cache') << cache
+        (
+            shortener_service
+            >> Relationship('create records with long URL, short URL, expiration')
+            >> database
+            << Relationship('deleted continue expirated URLs')
+            << expiration_manager
+        )
 
-		broker >> Relationship('sending data') << shortener_service
-
-		shortener_service >> Relationship('retrieving or writing data') << database \
-		>> Relationship('deleted continue expirated URLs') << expiration_manager
-
-		shortener_service >> Relationship('checking the cache for the necessary data') << cache
-
-		broker >> Relationship('sending data') << analytics_service \
-		>> Relationship('select data') << analytics_database
+        (
+            shortener_service
+            >> Relationship('create records and checking the short URL for existence')
+            << cache
+        )
